cc3e5e560f5673679527c8c4a0f705f7e9089633
sne94813 <35587822+sne94813@users.noreply.github.com>
2018-02-02 10:20:38 +0530

    Add files via upload

---
+++ snake_and_ladder_program.py
@@ -0,0 +1,51 @@
+import random
+count=0
+while(count<100):
+    n=input("press r to roll a dice")#r is the random dice no the computer gives
+    if(n=='r'):
+        r=random.randint(1,6)
+        count=count+r
+        print("ur random is",r)
+        print("ur count is",count)
+    if(count==8):
+        count=37
+        print("climb the ladder",count)#climbing the ladder
+    elif(count==13):
+        count=34
+        print("climb the ladder",count)
+    elif(count==40):
+        count=68
+        print("climb the ladder",count)
+    elif(count==52):
+        count=81
+        print("climb the ladder",count)
+    elif(count==76):
+        count=87
+        print("climb the ladder",count)
+    elif(count==11):
+        count=2
+        print("aah snake bite",count)
+    elif(count==25):
+        count=4
+        print("aah snake bite",count)
+    elif(count==38):
+        count=9
+        print("aah snake bite",count)#snake bite 
+    elif(count==65):
+        count=46
+        print("aah snake bite",count)
+    elif(count==89):
+        count=70
+        print("aah snake bite",count)
+    elif(count==93):
+        count=64
+        print("aah snake bite",count)
+    elif(count==100):
+        print("you are the winner")#if we get 100 then you are the winner
+    elif(count>100):
+        count=count-r #if it is more than 100 minus it to 100
+        print("try again")
+    else:
+        print("continue playing")#if u don't get 100 then continue
+    
+       
